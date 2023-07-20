37ec4c292c2be8ef28b14d2a9542e18c5e8cf823
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2023-07-20 13:20:05 +0530

    maintenance mode - version update

--- common_features_v14/common_features_v14/doctype/version_update/version_update.py
+++ common_features_v14/common_features_v14/doctype/version_update/version_update.py
@@ -3,44 +3,141 @@
 
 import frappe
 from frappe.model.document import Document
-from frappe.utils import get_url_to_form,now_datetime
 from datetime import datetime
+
 class VersionUpdate(Document):
+	def validate_maintenance_mode(self):
+		active_maint_modes = frappe.db.sql(f"""
+			SELECT 
+				vu.name
+			FROM `tabVersion Update` vu
+			WHERE
+				vu.name != '{self.name}' AND
+				vu.close_the_maintenance_mode = 0 AND
+				(	
+				    vu.under_maintenance_mode = 1 OR
+				    vu.maintenance_mode_warning_message = 1
+				)
+		""" ,as_dict = True)
+
+		if active_maint_modes:
+			frappe.throw(f"""
+					Please Close the active maintenance mode before opening new maintenance mode<br>
+					<ul>
+						{"".join([f'''<li><a href="/app/version-update/{mm.name}">{mm.name}</a></li>''' for mm in active_maint_modes])}
+					</ul>
+					""")
+
+	def validate(self):
+		if self.maintenance_mode_warning_message or self.under_maintenance_mode or self.close_the_maintenance_mode:
+			self.validate_maintenance_mode()
+
+		if not frappe.db.exists(self.doctype, self.name):
+			self.publish_update = True
+			return
+		
+		org_doc = frappe.get_doc(self.doctype, self.name)
+		if self.maintenance_mode_warning_message != org_doc.maintenance_mode_warning_message or self.under_maintenance_mode!=org_doc.under_maintenance_mode or self.close_the_maintenance_mode != org_doc.close_the_maintenance_mode:
+			self.publish_update = True
+		
+		elif self.pre_warning_message != org_doc.pre_warning_message or self.maintenance_mode_message!=org_doc.maintenance_mode_message:
+			self.publish_update = True
+		elif self.details != org_doc.details:
+			self.publish_update = True
+		else:
+			self.publish_update = False
+
 	def on_update(self):
-		if self.message_display == 1:
-			frappe.publish_realtime('custom-version_pre', {"link1":self.alert_message,"time":self.message_start_at})
+		if self.publish_update:
+			self.update_maintenance_mode()
+			frappe.db.set_value(self.doctype, self.name, 'publish_update', 0, update_modified=False)
+			self.reload()
 
-		if self.maintenance_check == 1:
-			frappe.publish_realtime('custom-version_maintenance', {"link2":self.maintenance_message})	
-		if self.close_the_maintenance_mode == 1:
-			frappe.publish_realtime('custom-version_success', {"link3":self.success_message})
+	def on_trash(self):
+		if not self.close_the_maintenance_mode and (self.under_maintenance_mode or self.maintenance_mode_warning_message or self.details):
+			self.clear_maintenance_mode()
+			frappe.msgprint("Maintenance mode message cleared.")
 
-# def on_update():
-# 	# frappe.publish_realtime('custom-version-update', {"link":get_url_to_form(self.doctype, self.name)})
+	def update_maintenance_mode(self):
+		css_indicators = {
+			'green': 'indicator-pill green',
+			'orange': 'indicator-pill orange',
+			'red': 'indicator-pill red',
+			'blue': 'indicator-pill blue'
+		}
+		if self.close_the_maintenance_mode:
+			frappe.publish_realtime('custom-version-update', {'message': self.success_message, 'refresh_button': True, 'indicator': css_indicators.get('green')})
+		
+		elif self.under_maintenance_mode:
+			frappe.publish_realtime('custom-version-update', {'message': self.maintenance_mode_message, 'refresh_button': False, 'indicator': css_indicators.get('red')})
+		
+		elif self.maintenance_mode_warning_message:
+			frappe.publish_realtime('custom-version-update', {'message': self.pre_warning_message, 'refresh_button': False, 'indicator': css_indicators.get('orange')})
+		
+		elif self.details:
+			frappe.publish_realtime('custom-version-update', {'message': self.details, 'refresh_button': True, 'indicator': css_indicators.get('blue')})
+		
+		else:
+			self.clear_maintenance_mode()
 	
-# 	doc_all = frappe.get_all("Version Update",{"maintenance_check":0,"message_start_at":["<=",now_datetime()]},pluck = "name")
-# 	for i in doc_all:
-# 		doc = frappe.get_doc("Version Update",i)
-# 		if doc.close_the_maintenance_mode == 0:	
-# 			frappe.publish_realtime('custom-version_maintenance', {"link2":doc.maintenance_message})
-# 			doc.maintenance_check = 1
-# 			doc.message_display = 0
-# 			doc.save()
-# 			doc.reload()
+	def clear_maintenance_mode(self):
+		frappe.publish_realtime('clear-custom-version-update')
+
+def update_maintenance_mode_on():
+	docs = frappe.db.sql("""
+		SELECT
+		    vu.name
+		FROM `tabVersion Update` vu
+		WHERE
+			vu.maintenance_mode_warning_message = 1 AND
+			IFNULL(vu.maintenance_mode_on_at, '') != '' AND
+			vu.maintenance_mode_on_at <= NOW() AND
+			vu.under_maintenance_mode = 0 AND
+			vu.close_the_maintenance_mode = 0
+	""", as_dict=True)
+
+	for doc in docs:
+		doc = frappe.get_doc('Version Update', doc.name)
+		doc.under_maintenance_mode = True
+		try:
+			doc.save()
+		except:
+			frappe.log_error()
 
 @frappe.whitelist()
-def refresh_warning():
-	doc = frappe.get_all("Version Update",{"message_display":1},["alert_message","message_start_at"])
-	if doc:
-		return doc[0].alert_message,doc[0].message_start_at
-
-@frappe.whitelist()
-def refresh_mainteance():
-	doc = frappe.get_all("Version Update",{"maintenance_check":1},["maintenance_message"])
-	if doc:
-		return doc[0].maintenance_message
-# @frappe.whitelist()
-# def refresh_success():
-# 	doc = frappe.get_all("Version Update",{"close_the_maintenance_mode":1},["success_message"])
-# 	if doc:
-# 		return doc[0].success_message
+def check_maintenance_mode():
+	active = []
+	try:
+		css_indicators = {
+				'green': 'indicator-pill green',
+				'orange': 'indicator-pill orange',
+				'red': 'indicator-pill red',
+				'blue': 'indicator-pill blue'
+			}
+		active = frappe.db.sql(f"""
+				SELECT 
+					CASE 
+						WHEN vu.under_maintenance_mode = 1
+							THEN IFNULL(vu.maintenance_mode_message, '')
+						ELSE IFNULL(vu.pre_warning_message, '')
+					END as message,
+					0 as refresh_button,
+					CASE 
+						WHEN vu.under_maintenance_mode = 1
+							THEN "{css_indicators.get('red') or ''}"
+						ELSE "{css_indicators.get('orange') or ''}"
+					END as indicator
+				FROM `tabVersion Update` vu
+				WHERE
+					vu.close_the_maintenance_mode = 0 AND
+					(	
+						vu.under_maintenance_mode = 1 OR
+						vu.maintenance_mode_warning_message = 1
+					)
+				LIMIT 1
+			""" ,as_list = True)
+	except:
+		frappe.log_error()
+	if active:
+		return active[0]
+	
--- common_features_v14/public/js/update.js
+++ common_features_v14/public/js/update.js
@@ -1,134 +1,62 @@
-// frappe.provide('common_features_v14');
-frappe.provide('common_features_pre');
-frappe.provide('common_features_success');
-frappe.provide('common_features_maintenance');
+frappe.provide('common_features_v14');
 
-// common_features_v14.update_indicator = function (link) {
-//     if (document.querySelector("[role='thirvu-update-indicator']")) {
-//         return
-//     }
-//     let header = document.querySelector(".main-section header[role='navigation']").parentElement
-//     var r = document.querySelector(':root');
-//     r.style.setProperty('--navbar-height', '95px');
-//     r.style.setProperty('--top-navhi', '40px');
-//     let update_version_txt = `<a class='version-update-link' href='${link}'>updated version</a>`
-//     header.prepend($(`
-//             <div class="app-update" role="thirvu-update-indicator">
-//                 <div class="app-update-refresh">
-//                     <div>
-//                         The application has been updated, please refresh this page to get a ${link?update_version_txt:'updated version'}
-//                     </div>
-//                     <button class="btn btn-primary" onclick=frappe.ui.toolbar.clear_cache()>Refresh</button>
-//                 </div>
-//             </div>`)[0]
-//     );
-// }
-
-common_features_pre.update_indicator_pre = function (link1,time) {
+common_features_v14.remove_update_indicator = function () {
     if (document.querySelector("[role='thirvu-update-indicator']")) {
-        return
+        var r = document.querySelector(':root');
+        document.querySelector("[role='thirvu-update-indicator']").remove();
+        r.style.setProperty('--navbar-height', '0');
+        r.style.setProperty('--top-navhi', '0');
     }
-    let header = document.querySelector(".main-section header[role='navigation']").parentElement
+}
+
+common_features_v14.update_indicator = function (message, refresh_button, indicator) {
+    common_features_v14.remove_update_indicator();
+
+    let header = document.querySelector(".main-section header[role='navigation']").parentElement;
+    
     var r = document.querySelector(':root');
-    r.style.setProperty('--navbar-height', '140px');
+    r.style.setProperty('--navbar-height', '95px');
     r.style.setProperty('--top-navhi', '40px');
+
     header.prepend($(`
-            <div class="app-update" role="thirvu-update-indicator">
-                <div class="app-update-refresh">
-                    <div>
-                        ${link1} ${time}
+            <div class="app-update-container" role="thirvu-update-indicator">
+                <div class="app-update app-update-transform-css ${indicator}">
+                    <div class="app-update-refresh">
+                        <div>
+                            ${message || ''}
+                        </div>
+                        <button class="btn btn-primary" style="${refresh_button?'':'display:none;'}" onclick=frappe.ui.toolbar.clear_cache()>Refresh</button>
                     </div>
                 </div>
             </div>`)[0]
     );
+
+    let ele = document.querySelector("[role='thirvu-update-indicator'] .app-update.app-update-transform-css");
+    if (ele) {
+        setTimeout(() => {
+            ele.classList.remove('app-update-transform-css')
+        }, 300)
+        
+    }
 }
 
-common_features_maintenance.update_indicator_maintenance = function (link2) {
-    if (document.querySelector("[role='thirvu-update-indicator']")) {
-        return
-    }
-    let header = document.querySelector(".main-section header[role='navigation']").parentElement
-    var r = document.querySelector(':root');
-    r.style.setProperty('--navbar-height', '140px');
-    r.style.setProperty('--top-navhi', '40px');
-    header.prepend($(`
-            <div class="app-update" role="thirvu-update-indicator">
-                <div class="app-update-refresh">
-                    <div>
-                        ${link2}
-                    </div>
-                </div>
-            </div>`)[0]
-    );
-}
-
-common_features_success.update_indicator_success = function (link3) {
-    if (document.querySelector("[role='thirvu-update-indicator']")) {
-        return
-    }
-    let header = document.querySelector(".main-section header[role='navigation']").parentElement
-    var r = document.querySelector(':root');
-    r.style.setProperty('--navbar-height', '140px');
-    r.style.setProperty('--top-navhi', '40px');
-    header.prepend($(`
-            <div class="app-update" role="thirvu-update-indicator">
-                <div class="app-update-refresh">
-                    <div>
-                        ${link3}
-                    </div>
-                    <button class="btn btn-primary" onclick=frappe.ui.toolbar.clear_cache()>Refresh</button>
-                </div>
-            </div>`)[0]
-    );
-}
 $(document).ready(function (frm) {
-    // frappe.realtime.on("custom-version-update", function ({ link }) {
-    //     common_features_v14.update_indicator(link)
-    // });
-    frappe.realtime.on("custom-version_pre", function ({ link1,time }) { 
-        common_features_pre.update_indicator_pre(link1,time)
+    frappe.realtime.on("custom-version-update", function ({message, refresh_button, indicator}) {
+        common_features_v14.update_indicator(message, refresh_button, indicator)
     });
-    frappe.realtime.on("custom-version_maintenance", function ({ link2 }) { 
-        common_features_maintenance.update_indicator_maintenance(link2)
+    frappe.realtime.on("clear-custom-version-update", function () {
+        common_features_v14.remove_update_indicator()
     });
-    frappe.realtime.on("custom-version_success", function ({ link3 }) { 
-        common_features_success.update_indicator_success(link3)
-        (`
-            <div class="app-update" role="thirvu-update-indicator">
-                <div class="app-update-refresh">
-                    <div>
-                        ${link3}
-                    </div>
-                    <button class="btn btn-primary" onclick=frappe.ui.toolbar.clear_cache()>Refresh</button>
-                </div>
-            </div>`)[0]
-    });
+   
     frappe.call({
-        method: "common_features_v14.common_features_v14.doctype.version_update.version_update.refresh_warning",
-        callback: function (r) { 
-            if (r.message[0]) {
-                common_features_pre.update_indicator_pre(r.message[0],r.message[1])
-            }
-        }
-    })
-    // frappe.call({
-    //     method: "common_features_v14.common_features_v14.doctype.version_update.version_update.refresh_success",
-    //     callback: function (r) { 
-    //         if (r.message) {
-    //             common_features_success.update_indicator_success(r.message)   
-    //         }
-    //     }
-    // })
-    frappe.call({
-        method: "common_features_v14.common_features_v14.doctype.version_update.version_update.refresh_mainteance",
+        method: "common_features_v14.common_features_v14.doctype.version_update.version_update.check_maintenance_mode",
         callback: function (r) { 
             if (r.message) {
-                common_features_maintenance.update_indicator_maintenance(r.message)   
+                common_features_v14.update_indicator(...(r.message))
             }
         }
-    })
+    });
 });
-
 
 frappe.db.get_single_value("Thirvu System Settings", "toggle_full_width").then((enabled) => {
     if(enabled){
@@ -136,14 +64,12 @@ frappe.db.get_single_value("Thirvu System Settings", "toggle_full_width").then((
             frappe.ui.toolbar.toggle_full_width()
         }
     }
-})
+});
+
 frappe.db.get_single_value("Thirvu System Settings", "hide_toggle_sidebar_in_list").then((enabled) => {
     if(enabled){
         localStorage.show_sidebar = false
     } else{
         localStorage.show_sidebar = true
     }
-})
-
-
-
+});
