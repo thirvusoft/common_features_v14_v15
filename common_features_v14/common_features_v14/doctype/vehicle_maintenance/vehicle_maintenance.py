a0d0e06f34e2b79eabe0ca3eed3ab0ce472c7311
rooban <rooban24samuel@gmail.com>
2024-03-28 16:41:37 +0530

    validation changes

--- common_features_v14/common_features_v14/doctype/vehicle_maintenance/vehicle_maintenance.py
+++ common_features_v14/common_features_v14/doctype/vehicle_maintenance/vehicle_maintenance.py
@@ -11,18 +11,21 @@ class VehicleMaintenance(Document):
 
 	def on_cancel(self):
 		odometer_cancel(self)
+		  
+	def validate(self):
+		odometer_validate(self)
 
 def odometer_update(self):
-    if self.vehicle_no:
-        vehicle = frappe.get_doc('Vehicle', self.vehicle_no) 
-        for det in self.maintenance_details:
-            for service_detail in vehicle.custom_service_maintanence_details:
-                if service_detail.service_type == det.service_type:
-                    service_detail.current_maintanence_odometer_value = self.service_done_at_odometer_value
-                    service_detail.next_maintanence_odometer_value = self.service_done_at_odometer_value + det.next_service_odometer_frequency
-                    
-        vehicle.save()
-        frappe.db.set_value("Vehicle", self.vehicle_no, "last_odometer", self.service_done_at_odometer_value)
+	if self.vehicle_no:
+		vehicle = frappe.get_doc('Vehicle', self.vehicle_no) 
+		for det in self.maintenance_details:
+			for service_detail in vehicle.custom_service_maintanence_details:
+				if service_detail.service_type == det.service_type:
+					service_detail.current_maintanence_odometer_value = self.service_done_at_odometer_value
+					service_detail.next_maintanence_odometer_value = self.service_done_at_odometer_value + det.next_service_odometer_frequency
+					
+		vehicle.save()
+		frappe.db.set_value("Vehicle", self.vehicle_no, "last_odometer", self.service_done_at_odometer_value)
 
 
 def odometer_cancel(self):
@@ -33,21 +36,24 @@ def odometer_cancel(self):
 			for det in self.maintenance_details:
 				if details.service_type == det.service_type:
 					details.current_maintanence_odometer_value= vehicle.last_odometer
-					vehicle.save()
-
-
+					details.next_maintanence_odometer_value = det.next_service_odometer_frequency - self.service_done_at_odometer_value
+		vehicle.save()
+		frappe.db.set_value("Vehicle", self.vehicle_no, "last_odometer", self.service_done_at_odometer_value)
 
 #Vehicle maintenance Doc filter
 
 @frappe.whitelist()
 def service_maintanence_details(vehicle_no):
-    vehicle=frappe.get_doc('Vehicle',vehicle_no)
-    ser_type=[]
-    for i in vehicle.custom_service_maintanence_details:
-        ser_type.append(i.service_type)
-    return ser_type
-    
+	vehicle=frappe.get_doc('Vehicle',vehicle_no)
+	ser_type=[]
+	for i in vehicle.custom_service_maintanence_details:
+		ser_type.append(i.service_type)
+	return ser_type
+	
 
+def odometer_validate(self):
+	 if self.service_done_at_odometer_value < self.current_odometer:
+			 frappe.throw("Service done at Odometer value should be greater than Current Odometer")
 
 
 
